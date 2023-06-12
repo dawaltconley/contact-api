b4d0eb4192860689bc61a09ee665a7a603bbf0f5
Dylan Awalt-Conley <web@dylan.ac>
2023-06-12 15:49:23 -0400

    parse form data using busboy

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,4 +1,30 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import busboy from 'busboy';
+
+const parseFormData = (
+  body: string,
+  headers: APIGatewayProxyEvent['headers'],
+): Promise<Record<string, string>> =>
+  new Promise((resolve, reject) => {
+    const parsed: Record<string, string> = {};
+    const bb = busboy({
+      headers: Object.entries(headers).reduce<
+        Record<string, string | undefined>
+      >(
+        (headers, [k, v]) => ({
+          ...headers,
+          [k.toLowerCase()]: v,
+        }),
+        {},
+      ),
+    });
+    bb.on('field', (name, value) => {
+      parsed[name] = value;
+    });
+    bb.on('close', () => resolve(parsed));
+    bb.on('error', reject);
+    bb.end(body);
+  });
 
 /**
  *
@@ -14,11 +40,17 @@ export const lambdaHandler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
   try {
+    if (!event.body) throw new Error('Event missing a body.');
+    const data = await parseFormData(event.body, event.headers);
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world',
-      }),
+      headers: {
+        'Access-Control-Allow-Headers':
+          'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+      },
+      body: JSON.stringify(data),
     };
   } catch (err) {
     console.log(err);
